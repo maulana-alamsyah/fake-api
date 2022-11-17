528de60a32fd3d29715073bdd5bc030b3ef7b9d2
maulana-alamsyah <maulanalamsyah@gmail.com>
2022-11-17 17:14:19 +0700

    remove cors addvia header response

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
-const cors = require('cors');
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 
 const PORT = process.env.PORT || 3000;
-
-var corsOptions = {
-    origin: '*'
-}
 
 // /!\ Bind the router db to the app
 app.db = router.db
@@ -17,7 +12,11 @@ app.db = router.db
 // You must apply the auth middleware before the router
 app.use(auth)
 app.use(router)
-app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 app.listen(PORT, () => {
     console.log('JSON Server is running');
 })
