b42ce797a3bac919e60323fd26a0a9df6e6bd7f4
maulana-alamsyah <maulanalamsyah@gmail.com>
2022-11-17 17:22:51 +0700

    cors add origin *

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+const cors = require('cors');
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -12,11 +13,7 @@ app.db = router.db
 // You must apply the auth middleware before the router
 app.use(auth)
 app.use(router)
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-});
+app.use(cors({origin: '*'}))
 app.listen(PORT, () => {
     console.log('JSON Server is running');
 })
