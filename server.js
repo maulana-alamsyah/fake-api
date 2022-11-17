817203339b84edebae3d5ee3524c3873ffdd3aa2
maulana-alamsyah <maulanalamsyah@gmail.com>
2022-11-17 13:32:12 +0700

    change port

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const auth = require('json-server-auth')
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 
+const PORT = process.env.PORT || 3000;
+
 // /!\ Bind the router db to the app
 app.db = router.db
 
 // You must apply the auth middleware before the router
 app.use(auth)
 app.use(router)
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('JSON Server is running');
 })
