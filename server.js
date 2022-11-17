fa213dd0ef1e08af50e086fbb33490576ed36a11
maulana-alamsyah <maulanalamsyah@gmail.com>
2022-11-17 17:41:57 +0700

    asd

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const app = jsonServer.create()
 const router = jsonServer.router('db.json')
 
 const PORT = process.env.PORT || 3000;
-var allowOrigin = ['http://localhost', '*'];
 
 // /!\ Bind the router db to the app
 app.db = router.db
 
+app.use(cors())
 // You must apply the auth middleware before the router
 app.use(auth)
 app.use(router)
-app.use(cors({origin: allowOrigin}))
 app.listen(PORT, () => {
     console.log('JSON Server is running');
 })
